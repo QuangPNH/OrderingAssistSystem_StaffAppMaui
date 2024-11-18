923e818c999aebe9af2c825426e27deb5b204e00
Vipxpert <vipxpert3721@gmail.com>
2024-11-19 00:56:11 +0700

    Pending List has topping, sugar, and ice  fields

--- OrderingAssistSystem_StaffApp/Models/OrderDetail.cs
+++ OrderingAssistSystem_StaffApp/Models/OrderDetail.cs
@@ -11,8 +11,11 @@ namespace OrderingAssistSystem_StaffApp.Models
         public int? OrderId { get; set; }
         public bool? Status { get; set; }
         public string? Description { get; set; }
+		public string? Sugar { get; set; }
+		public string? Ice { get; set; }
+		public string? Topping { get; set; }
 
-        public virtual MenuItem? MenuItem { get; set; }
+		public virtual MenuItem? MenuItem { get; set; }
         public virtual Order? Order { get; set; }
     }
 }
