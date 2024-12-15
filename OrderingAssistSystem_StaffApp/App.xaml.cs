6f710f597e906a2eddc251b1913c38a2e4345628
QuangPNH <quangpnhhe170627@fpt.edu.vn>
2024-12-16 04:08:08 +0700

    add support for notification for client to staff and staff to bartender

--- OrderingAssistSystem_StaffApp/App.xaml.cs
+++ OrderingAssistSystem_StaffApp/App.xaml.cs
@@ -1,74 +1,99 @@
-﻿using OrderingAssistSystem_StaffApp.Models;
+﻿using AzzanOrder.Data.Models;
+using Newtonsoft.Json;
+using OrderingAssistSystem_StaffApp.Models;
 using OrderingAssistSystem_StaffApp.Services;
 using Plugin.LocalNotification;
+using System.Net.Http.Json;
 namespace OrderingAssistSystem_StaffApp
 {
-	public partial class App : Application
-	{
-		public static PageCache PageCache { get; private set; }
-		readonly IPushDemoNotificationActionService _actionService;
-		public App(IPushDemoNotificationActionService service)
-		{
-			InitializeComponent();
-			_actionService = service;
-			_actionService.ActionTriggered += NotificationActionTriggered;
-			INotificationRegistrationService serviceNoti = DependencyService.Get<INotificationRegistrationService>();
-			MainPage = new AppShell();
-		}
-		void NotificationActionTriggered(object sender, OasStaffAppAction e)
-		{
-			ShowActionAlert(e);
-		}
+    public partial class App : Application
+    {
+        private readonly HttpClient httpClient;
+        public static PageCache PageCache { get; private set; }
+        readonly IPushDemoNotificationActionService _actionService;
+        public App(IPushDemoNotificationActionService service)
+        {
+            InitializeComponent();
+            _actionService = service;
+            _actionService.ActionTriggered += NotificationActionTriggered;
+            INotificationRegistrationService serviceNoti = DependencyService.Get<INotificationRegistrationService>();
+            MainPage = new AppShell();
+            httpClient = new HttpClient();
+        }
+        void NotificationActionTriggered(object sender, OasStaffAppAction e)
+        {
+            ShowActionAlert(e);
+        }
 
-		void ShowActionAlert(OasStaffAppAction action)
-		{
-			PendingOrderViewModel _pendingOrderViewModel = new PendingOrderViewModel();
-			ItemToMakeListViewModel itemToMakeListViewModel = new ItemToMakeListViewModel();
-			var a = action.ToString();
-			if (action.ToString().Equals("Confirm"))
-			{
-				MainThread.BeginInvokeOnMainThread(() =>
-				{
-					var notification = new NotificationRequest
-					{
-						Title = "Notice",
-						Description = $"New orders need to be confirmed paid.",
-						ReturningData = "Dummy data",
-						NotificationId = 1337
-					};
-					LocalNotificationCenter.Current.Show(notification);
-				});
-				_pendingOrderViewModel.LoadOrders();
-			}
-			else if (action.ToString().Equals("OrderSuccesses"))
-			{
-				MainThread.BeginInvokeOnMainThread(() =>
-				{
-					var notification = new NotificationRequest
-					{
-						Title = "Notice",
-						Description = $"New items to make incoming.",
-						ReturningData = "Dummy data",
-						NotificationId = 1337
-					};
-					LocalNotificationCenter.Current.Show(notification);
-				});
-				itemToMakeListViewModel.LoadOrderDetails();
-			}
+        public async Task<StaffNotiChannel> GetLatestStaffNotiChannelAsync(int managerId)
+        {
+            ConfigApi configApi = new ConfigApi();
+            var url = configApi._apiUrl + $"StaffNotiChannels/latest/{managerId}";
+            return await httpClient.GetFromJsonAsync<StaffNotiChannel>(url);
+        }
+        async void ShowActionAlert(OasStaffAppAction action)
+        {
+            PendingOrderViewModel _pendingOrderViewModel = new PendingOrderViewModel();
+            ItemToMakeListViewModel itemToMakeListViewModel = new ItemToMakeListViewModel();
+            var a = action.ToString();
+            if (action.ToString().Equals("Confirm"))
+            {
+                var loginInfo = Preferences.Get("LoginInfo", string.Empty);
+                Employee emp = JsonConvert.DeserializeObject<Employee>(loginInfo);
+                if (emp != null)
+                {
+                    StaffNotiChannel _latestStaffNoti = await GetLatestStaffNotiChannelAsync(emp.ManagerId.Value);
+                    if (_latestStaffNoti != null && _latestStaffNoti.IsSent == false)
+                    {
+                        MainThread.BeginInvokeOnMainThread(() =>
+                        {
+                            var notification = new NotificationRequest
+                            {
+                                Title = "Notice",
+                                Description = _latestStaffNoti.Message,
+                                ReturningData = "Dummy data",
+                                NotificationId = 1337
+                            };
+                            LocalNotificationCenter.Current.Show(notification);
+                        });
+                    }
+                }
+                _pendingOrderViewModel.LoadOrders();
+            }
+            else if (action.ToString().Equals("OrderSuccesses"))
+            {
+                var loginInfo = Preferences.Get("LoginInfo", string.Empty);
+                Employee emp = JsonConvert.DeserializeObject<Employee>(loginInfo);
+                if (emp != null && emp.RoleId == 3)
+                {
+                    MainThread.BeginInvokeOnMainThread(() =>
+                    {
+                        var notification = new NotificationRequest
+                        {
+                            Title = "Notice",
+                            Description = "New Order Being Made !.",
+                            ReturningData = "Dummy data",
+                            NotificationId = 1337
+                        };
+                        LocalNotificationCenter.Current.Show(notification);
+                    });
+                }
+                itemToMakeListViewModel.LoadOrderDetails();
+            }
 			/*else
-			{
-				MainThread.BeginInvokeOnMainThread(() =>
-			{
-				var notification = new NotificationRequest
-				{
-					Title = "Notice",
-					Description = "Unspecified.",
-					ReturningData = "Dummy data",
-					NotificationId = 1337
-				};
-				LocalNotificationCenter.Current.Show(notification);
-			});
+            {
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    var notification = new NotificationRequest
+                    {
+                        Title = "Notice",
+                        Description = "Unspecified.",
+                        ReturningData = "Dummy data",
+                        NotificationId = 1337
+                    };
+                    LocalNotificationCenter.Current.Show(notification);
+                });
 			}*/
-		}
-	}
+        }
+    }
 }
--- OrderingAssistSystem_StaffApp/Services/PushDemoNotificationActionService.cs
+++ OrderingAssistSystem_StaffApp/Services/PushDemoNotificationActionService.cs
@@ -8,41 +8,41 @@ using System.Threading.Tasks;
 namespace OrderingAssistSystem_StaffApp.Services
 {
 
-	public class PushDemoNotificationActionService : IPushDemoNotificationActionService
-	{
-		readonly Dictionary<string, OasStaffAppAction> _actionMappings = new Dictionary<string, OasStaffAppAction>
-	{
-		{ "action_a", OasStaffAppAction.ActionA },
-		{ "action_b", OasStaffAppAction.ActionB },
-			{ "OrderSuccesses", OasStaffAppAction.OrderSuccesses },
-{ "Confirm", OasStaffAppAction.Confirm }
-	};
+    public class PushDemoNotificationActionService : IPushDemoNotificationActionService
+    {
+        readonly Dictionary<string, OasStaffAppAction> _actionMappings = new Dictionary<string, OasStaffAppAction>
+    {
+        { "action_a", OasStaffAppAction.ActionA },
+        { "action_b", OasStaffAppAction.ActionB },
+        { "OrderSuccesses", OasStaffAppAction.OrderSuccesses },
+        { "Confirm", OasStaffAppAction.Confirm }
+    };
 
-		public event EventHandler<OasStaffAppAction> ActionTriggered = delegate { };
+        public event EventHandler<OasStaffAppAction> ActionTriggered = delegate { };
 
-		public void TriggerAction(string action)
-		{
+        public void TriggerAction(string action)
+        {
 
 
-			if (!_actionMappings.TryGetValue(action, out var pushDemoAction))
-				return;
-			List<Exception> exceptions = new List<Exception>();
+            if (!_actionMappings.TryGetValue(action, out var pushDemoAction))
+                return;
+            List<Exception> exceptions = new List<Exception>();
 
-			foreach (var handler in ActionTriggered?.GetInvocationList())
-			{
-				try
-				{
-					handler.DynamicInvoke(this, pushDemoAction);
-				}
-				catch (Exception ex)
-				{
-					exceptions.Add(ex);
-				}
-			}
+            foreach (var handler in ActionTriggered?.GetInvocationList())
+            {
+                try
+                {
+                    handler.DynamicInvoke(this, pushDemoAction);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
 
-			if (exceptions.Any())
-				throw new AggregateException(exceptions);
-		}
-	}
+            if (exceptions.Any())
+                throw new AggregateException(exceptions);
+        }
+    }
 
 }
